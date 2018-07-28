16c63d2524462397d2c081c2cfb9cd674bdd2bab
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-28 12:56:04 +0200

    app.component.ts Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+// import { Observable } from 'rxjs'; //no!
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firestore';
+
+  constructor(private afs: AngularFirestore){
+
+  }
+
+  ngOnInit(){
+
+  }
+
 }
